18c05ebd925f899dd1d7248132593b0e59469c0a
s-r-aman <s.r.aman.sra@gmail.com>
2017-10-25 12:43:47 +0530

    Added header with navbar and footer.
    This commit adds the navbar with the header and footer and some other ideas of the content on home page.

--- script/app.js
+++ script/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  var targetPos = $(".scrollDiv").offset().top;
+$(document).ready(function() {
+  /*var targetPos = $(".scrollDiv").offset().top;
 
   $(window).resize(function(){
     var targetPos = $(".scrollDiv").offset().top;
@@ -28,10 +28,176 @@ $(document).ready(function(){
       $('nav').css('box-shadow','0px 5px 1px rgba(0, 4, 0, 0.28)');
     }
 
+
+  })
+  */
+  /* To know whether the user is scrolling up or down*/
+  var position = $(window).scrollTop();
+
+  $(window).scroll(function() {
+    currentScrollPos = $(this).scrollTop();
+    if (currentScrollPos > position) {
+      console.log('Scrolling downwards');
+      $('header').addClass("hide");
+    } else {
+      console.log('Scrolling upwards');
+      $('header').removeClass("hide");
+    }
+    position = currentScrollPos;
   })
 
+  /* Making the dropdown menu*/
+  var click1 = 0;
+  $(".drop-academics").click(function() {
 
-})
+    if (!click1) {
+      if(click2 || click3){
+        if (click2) {
+          $("#student-section").animate({
+            top: "-343px"
+          });
+          $('.drop-student').toggleClass('activeLine activeText')
+          click2 = 0;
+          $("#academics-section").animate({
+            top: "96px"
+          });
+          $('.drop-academics').toggleClass('activeLine activeText')
+          click1 = 1;
+        }else{
+          $("#manage-section").animate({
+            top: "-343px"
+          });
+          $('.drop-manage').toggleClass('activeLine activeText')
+          click3 = 0;
+          $("#academics-section").animate({
+            top: "96px"
+          });
+          $('.drop-academics').toggleClass('activeLine activeText')
+          click1 = 1;
+        }
+
+      }else{
+        $("#academics-section").animate({
+          top: "96px"
+        });
+
+        $('.drop-academics').toggleClass('activeLine activeText')
+
+        click1 = 1;
+      }
+    } else {
+      $("#academics-section").animate({
+        top: "-343px"
+      });
+      $('.drop-academics').toggleClass('activeLine activeText')
+      click1 = 0;
+    }
+
+  });
+
+
+  var click2 = 0;
+  $(".drop-student").click(function() {
+
+    if (!click2) {
+      if(click1 || click3){
+        if (click1) {
+          $("#academics-section").animate({
+            top: "-343px"
+          });
+          $('.drop-academics').toggleClass('activeLine activeText')
+          click1 = 0;
+          $("#student-section").animate({
+            top: "96px"
+          });
+          $('.drop-student').toggleClass('activeLine activeText')
+          click2 = 1;
+        }else{
+          $("#manage-section").animate({
+            top: "-343px"
+          });
+          $('.drop-manage').toggleClass('activeLine activeText')
+          click3 = 0;
+          $("#student-section").animate({
+            top: "96px"
+          });
+          $('.drop-student').toggleClass('activeLine activeText')
+          click2 = 1;
+        }
+
+      }else{
+        $("#student-section").animate({
+          top: "96px"
+        });
+        $('.drop-student').toggleClass('activeLine activeText')
+        click2 = 1;
+      }
+    } else {
+      $("#student-section").animate({
+        top: "-343px"
+      });
+      $('.drop-student').toggleClass('activeLine activeText')
+      click2 = 0;
+    }
+
+  });
+
+  var click3 = 0;
+  $(".drop-manage").click(function() {
+
+    if (!click3) {
+      if(click1 || click2){
+        if (click1) {
+          $("#academics-section").animate({
+            top: "-343px"
+          });
+          $('.drop-academics').toggleClass('activeLine activeText')
+          click1 = 0;
+          $("#manage-section").animate({
+            top: "96px"
+          });
+          $('.drop-manage').toggleClass('activeLine activeText')
+          click3 = 1;
+        }else{
+          $("#student-section").animate({
+            top: "-343px"
+          });
+          $('.drop-student').toggleClass('activeLine activeText')
+          click2 = 0;
+          $("#manage-section").animate({
+            top: "96px"
+          });
+          $('.drop-manage').toggleClass('activeLine activeText')
+          click3 = 1;
+        }
+
+      }else{
+        $("#manage-section").animate({
+          top: "96px"
+        });
+        $('.drop-manage').toggleClass('activeLine activeText')
+        click3 = 1;
+      }
+
+
+
+    } else {
+      $("#manage-section").animate({
+        top: "-343px"
+      });
+      $('.drop-manage').toggleClass('activeLine activeText')
+      click3 = 0;
+    }
+
+  });
+
+  $(".prevent").click(function(e) {
+    e.preventDefault();
+  });
+
+
+});
+
 
 
 /*$(window).scroll(function() {
